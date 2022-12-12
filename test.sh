2927cba8197e7c1873a918b403ed026b951bdc7a
Alex Garcia <alexsebastian.garcia@gmail.com>
2022-12-12 14:48:58 -0700

    pipfile, cleanup

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-hyperfine --prepare 'rm processed/*.db || true' --max-runs 2 \
+hyperfine --prepare 'rm processed/*.db || true' \
   'ogr2ogr -f SQLite -dsco SPATIALITE=YES -lco geometry_name=geometry -nln census_2020 processed/ogr2ogr.db processed/counties_2020.geojson' \
   'sqlite3x processed/json.db ".read tests/sqlite-json/load.sql"' \
   'pipenv run geojson-to-sqlite processed/gts.db census_2020 processed/counties_2020.geojson --spatialite' \
